a35f165f60e35d98a24638d5960cd9d331e3b2ed
shafik720 <shafikrasel5@gmail.com>
2022-12-28 18:49:30 +0600

    sdf

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import { useEffect, useState } from 'react';
 function App() {
   const[user, setUser] = useState([]);
   useEffect(()=>{
-    fetch('http://localhost:5000/users')
+    fetch('http://localhost:2100/users')
     .then(res=>res.json())
     .then(data=>setUser(data))
   },[])
   return (
     <div className="App">
       <h2>User Found : {user.length} </h2>
+      {
+        user.map(index=><h2>{index.name}</h2>)
+      }
     </div>
   );
 }
