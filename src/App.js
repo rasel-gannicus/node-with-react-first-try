a848df55000b4f5751363218047bbfc0aa881d1e
shafik720 <shafikrasel5@gmail.com>
2022-12-29 14:32:49 +0600

    dsfsdf

--- src/App.js
+++ src/App.js
@@ -9,9 +9,30 @@ function App() {
     .then(res=>res.json())
     .then(data=>setUser(data))
   },[])
+
+  function handleSubmit(e){
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const user = {name, email};
+    fetch('http://localhost:2100/user',{
+      method : 'POST',
+      headers : {
+        'content-type' : 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res=>res.json())
+    .then(data=>console.log(data))
+  }
   return (
     <div className="App">
       <h2>User Found : {user.length} </h2>
+      <form action="" onSubmit={handleSubmit}>
+        <input type="email" name="email" placeholder='Your email' id="" />
+        <input type="text" name="name" id="" />
+        <button type="submit">Submit</button>
+      </form>
       {
         user.map(index=><h2>{index.name}</h2>)
       }
