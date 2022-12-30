a6e7d9e19276215ebfd4563168c87f6614faca2d
shafik720 <shafikrasel5@gmail.com>
2022-12-30 22:15:24 +0600

    dfds

--- src/App.js
+++ src/App.js
@@ -3,15 +3,10 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const[user, setUser] = useState([]);
-  useEffect(()=>{
-    fetch('http://localhost:5000/users')
-    .then(res=>res.json())
-    .then(data=>setUser(data))
-  },[])
+  
   return (
     <div className="App">
-      <h2>User Found : {user.length} </h2>
+      <h2>Blank page</h2>
     </div>
   );
 }
