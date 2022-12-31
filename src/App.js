30a1e31acebf4908466a92c89d5b5cb18a9e5af3
shafik720 <shafikrasel5@gmail.com>
2022-12-31 13:44:02 +0600

    trying node basic

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-  
+  const[players, setPlayers] = useState([]);
+  useEffect(()=>{
+    fetch('http://localhost:3200/user')
+    .then(res=>res.json())
+    .then(data=>setPlayers(data))
+  },[players])
   return (
     <div className="App">
-      <h2>Blank page</h2>
+      <h2>User Found : {players.length} </h2>
     </div>
   );
 }
