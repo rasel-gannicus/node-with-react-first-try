0f524e3ef6675660e52f1eb08ba0bc532695d467
shafik720 <shafikrasel5@gmail.com>
2022-12-31 21:40:40 +0600

    trying mongodb firsttime

--- src/Components/AddUser/AddUser.js
+++ src/Components/AddUser/AddUser.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
 const AddUser = () => {
+
+    function handleSubmit(e){
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const user = {name, email};
+
+        // send data to the server
+        fetch('http://localhost:5000/user',{
+            method : 'POST',
+            headers : {
+                'content-type' : 'application/json'
+            },
+            body : JSON.stringify(user)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log('success', data);
+            alert('user added successfully');
+        })
+    }
     return (
         <div>
             <h2>Add User</h2>
+            <form action="" onSubmit={handleSubmit} >
+                <input type="email" name="email" id="" /> <br />
+                <input type="text" name="name" id="" /> <br />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     );
 };
