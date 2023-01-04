282b9eec896429a235951164c0be0e787933635d
shafik720 <shafikrasel5@gmail.com>
2023-01-04 20:55:40 +0600

    update user in mongodb

--- src/Components/UpdateUser/UpdateUser.js
+++ src/Components/UpdateUser/UpdateUser.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const UpdateUser = () => {
     const {id} = useParams();
+    const [users, setUsers] = useState([]);
+    useEffect(()=>{
+        const url = `http://localhost:5000/user/${id}`;
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=>setUsers(data))
+    },[users])
     return (
         <div>
             <h2>Update user : {id} </h2>
+            <h2>{users.name}</h2>
         </div>
     );
 };
