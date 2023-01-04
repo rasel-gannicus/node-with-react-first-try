97ca78b067bfb7bba2d40bafc37310422e2fea4e
shafik720 <shafikrasel5@gmail.com>
2023-01-04 21:05:12 +0600

    update user in mongodb

--- src/Components/UpdateUser/UpdateUser.js
+++ src/Components/UpdateUser/UpdateUser.js
@@ -9,11 +9,36 @@ const UpdateUser = () => {
         fetch(url)
         .then(res=>res.json())
         .then(data=>setUsers(data))
-    },[users])
+    },[users]);
+
+    function handleUpdate(e){
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const user = {name, email};
+
+        // send data to the server
+        fetch('http://localhost:5000/user',{
+            method : 'POST',
+            headers : {
+                'content-type' : 'application/json'
+            },
+            body : JSON.stringify(user)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log('success', data);
+            alert('user added successfully');
+        })
+    }
     return (
         <div>
-            <h2>Update user : {id} </h2>
-            <h2>{users.name}</h2>
+            <h2>Update User</h2>
+            <form action="" onSubmit={handleUpdate} >
+                <input type="email" name="email" id="" /> <br />
+                <input type="text" name="name" id="" /> <br />
+                <button type="submit">Update</button>
+            </form>
         </div>
     );
 };
