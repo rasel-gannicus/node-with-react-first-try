b1b95a4a13d6b5f100caab52db9ea46234ad67b4
shafik720 <shafikrasel5@gmail.com>
2023-01-05 22:59:53 +0600

    trying to delete

--- src/Components/SecondPhase/SecondPhase.js
+++ src/Components/SecondPhase/SecondPhase.js
@@ -2,16 +2,30 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const SecondPhase = () => {
-    const[users, setUsers] = useState([]);
-    useEffect(()=>{
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
         const url = `http://localhost:5000/user`;
         fetch(url)
-        .then(res=>res.json())
-        .then(data=>setUsers(data));        
-    },[])
+            .then(res => res.json())
+            .then(data => setUsers(data));
+    }, [])
 
-    function deleteUser(id){
-        
+    function deleteUser(id) {
+        const proceed = window.confirm('Are you sure to delete ? ');
+        if (proceed) {
+            let url = `http://localhost:5000/user/${id}`;
+            fetch(url, {
+                method: 'DELETE',
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                if(data.deletedCount > 0 ){
+                    let remaining = users.filter(index=>index._id != id);
+                    setUsers(remaining);
+                }
+            })
+        }
+
     }
     return (
         <div>
@@ -19,9 +33,9 @@ const SecondPhase = () => {
             <Link to='/addUserSecond'>Add Users</Link>
             <h2>Total Users found till now : {users.length} </h2>
             {
-                users.map(index=> <h2 index={index} key={index._id}> {index.name}  
-                    <button onClick={()=>deleteUser(index._id)}>Delete</button>
-                </h2> )
+                users.map(index => <h2 index={index} key={index._id}> {index.name}
+                    <button onClick={() => deleteUser(index._id)}>Delete</button>
+                </h2>)
             }
         </div>
     );
