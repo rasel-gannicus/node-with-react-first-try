153564ea2d9e342ec994693ffeec4e7c4cf34b22
shafik720 <shafikrasel5@gmail.com>
2023-01-05 23:35:20 +0600

    trying to update user

--- src/Components/SecondPhase/UpdateSecondUser/UpdateSecondUser.js
+++ src/Components/SecondPhase/UpdateSecondUser/UpdateSecondUser.js
@@ -8,12 +8,39 @@ const UpdateSecondUser = () => {
         fetch(`http://localhost:5000/user/${id}`)
         .then(res=>res.json())
         .then(data=>setUsers(data))
-    },[])
+    },[]);
+
+    function handleSubmit(e){
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const user = {name, email};
+        
+        fetch(`http://localhost:5000/user/${id}`,{
+            method : 'PUT',
+            headers : {
+                'content-type' : 'application/json'
+            },
+            body : JSON.stringify(user)            
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            setUsers(data);
+            alert('user updated successfully');
+        })
+    }
     return (
         <div>
             <h2> User found  for : {id} </h2>
             <h1>Name : {users.name} </h1>
             <p>Email : {users.email} </p>
+
+            <h2>Update User : </h2>
+            <form action=""  onSubmit={handleSubmit}>
+                <input type="email" name="email" id="" /> <br />
+                <input type="text" name="name" id="" /> <br />
+                <button type="submit">Update</button>
+            </form>
         </div>
     );
 };
