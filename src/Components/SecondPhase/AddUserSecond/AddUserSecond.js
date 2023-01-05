8e8a6255cb38340bddde76852e60f4e79c917d7d
shafik720 <shafikrasel5@gmail.com>
2023-01-05 18:33:40 +0600

    trying node basic

--- src/Components/SecondPhase/AddUserSecond/AddUserSecond.js
+++ src/Components/SecondPhase/AddUserSecond/AddUserSecond.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 const AddUserSecond = () => {
+    function handleSubmit(e){
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const user = {name, email};
+        console.log(user);
+        return;
+    }
     return (
         <div>
             <h2>Add User for 2nd time</h2>
-            <form action=""  >
+            <form action=""  onSubmit={handleSubmit}>
                 <input type="email" name="email" id="" /> <br />
                 <input type="text" name="name" id="" /> <br />
                 <button type="submit">Submit</button>
