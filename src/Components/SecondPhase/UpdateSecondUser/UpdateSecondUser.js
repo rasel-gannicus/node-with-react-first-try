cb0a9e39645e173f0c9b6ee9feb57d522393f49a
shafik720 <shafikrasel5@gmail.com>
2023-01-05 23:27:30 +0600

    trying to update user

--- src/Components/SecondPhase/UpdateSecondUser/UpdateSecondUser.js
+++ src/Components/SecondPhase/UpdateSecondUser/UpdateSecondUser.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 const UpdateSecondUser = () => {
     const {id} = useParams();
+    const[users, setUsers] = useState([]);
+    useEffect(()=>{
+        fetch(`http://localhost:5000/user/${id}`)
+        .then(res=>res.json())
+        .then(data=>setUsers(data))
+    },[])
     return (
         <div>
             <h2> User found  for : {id} </h2>
+            <h1>Name : {users.name} </h1>
+            <p>Email : {users.email} </p>
         </div>
     );
 };
