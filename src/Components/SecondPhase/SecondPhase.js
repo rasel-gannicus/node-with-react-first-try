48fd30c572bb8962fdbcc15c796f945eda7fbab6
shafik720 <shafikrasel5@gmail.com>
2023-01-05 20:03:08 +0600

    trying node basic

--- src/Components/SecondPhase/SecondPhase.js
+++ src/Components/SecondPhase/SecondPhase.js
@@ -9,13 +9,19 @@ const SecondPhase = () => {
         .then(res=>res.json())
         .then(data=>setUsers(data));        
     },[])
+
+    function deleteUser(id){
+        console.log(id);
+    }
     return (
         <div>
             <h2>Second Try starts here</h2>
             <Link to='/addUserSecond'>Add Users</Link>
             <h2>Total Users found till now : {users.length} </h2>
             {
-                users.map(index=> <h2 index={index} key={index._id}> {index.name} </h2> )
+                users.map(index=> <h2 index={index} key={index._id}> {index.name}  
+                    <button onClick={()=>deleteUser(index._id)}>Delete</button>
+                </h2> )
             }
         </div>
     );
