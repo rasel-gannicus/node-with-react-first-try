65548951e9ae21fc592770a2c0849a6ce41f3b15
shafik720 <shafikrasel5@gmail.com>
2023-01-05 20:01:45 +0600

    trying node basic

--- src/Components/SecondPhase/AddUserSecond/AddUserSecond.js
+++ src/Components/SecondPhase/AddUserSecond/AddUserSecond.js
@@ -6,8 +6,18 @@ const AddUserSecond = () => {
         const name = e.target.name.value;
         const email = e.target.email.value;
         const user = {name, email};
-        console.log(user);
-        return;
+        
+        fetch('http://localhost:5000/user',{
+            method : 'POST',
+            headers : {
+                'content-type' : 'application/json'
+            },
+            body : JSON.stringify(user)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            alert('user added successfully');
+        })
     }
     return (
         <div>
--- src/Components/SecondPhase/SecondPhase.js
+++ src/Components/SecondPhase/SecondPhase.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const SecondPhase = () => {
+    const[users, setUsers] = useState([]);
+    useEffect(()=>{
+        const url = `http://localhost:5000/user`;
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=>setUsers(data));        
+    },[])
     return (
         <div>
             <h2>Second Try starts here</h2>
             <Link to='/addUserSecond'>Add Users</Link>
+            <h2>Total Users found till now : {users.length} </h2>
+            {
+                users.map(index=> <h2 index={index} key={index._id}> {index.name} </h2> )
+            }
         </div>
     );
 };
